f854fb33e5a08839d6900435388815c8c889dd78
youlheek <youlheek@gmail.com>
2024-12-11 14:11:34 +0900

    feat : [017-DEPLOY] 결제하기 - jar 파일 이용하여 Docker 이미지 생성 - AWS 이용하여 서비스

---
+++ user-api/src/main/java/com/zerobase/cms/user/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.zerobase.cms.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public String hello() {
+		return "hello user-api";
+	}
+}
+
