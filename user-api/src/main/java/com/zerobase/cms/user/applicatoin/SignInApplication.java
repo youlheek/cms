909e6ab48f9d473f535b0b51b2585860b7a4401a
youlheek <youlheek@gmail.com>
2024-12-18 01:58:50 +0900

    feat : [018-ADVANCEMENT] user-api 1. Spring Security 세팅 및 설정파일(Config)적용 2. 로그인시 Jwt access token, refresh token 적용 2-1. refresh Token을 Redis에 저장 및 HttpOnly Cookie에 설정 3. Authentication을 통해 사용자 인증 정보 관리

--- user-api/src/main/java/com/zerobase/cms/user/applicatoin/SignInApplication.java
+++ user-api/src/main/java/com/zerobase/cms/user/applicatoin/SignInApplication.java
@@ -1,17 +1,20 @@
 package com.zerobase.cms.user.applicatoin;
 
+import com.zerobase.cms.user.domain.AuthResponse;
 import com.zerobase.cms.user.domain.SignInForm;
 import com.zerobase.cms.user.domain.model.Customer;
 import com.zerobase.cms.user.domain.model.Seller;
 import com.zerobase.cms.user.exception.CustomException;
 import com.zerobase.cms.user.service.customer.CustomerService;
 import com.zerobase.cms.user.service.seller.SellerService;
-import com.zerobase.domain.common.UserType;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import static com.zerobase.cms.user.exception.ErrorCode.LOGIN_CHECK_FAIL;
 import static com.zerobase.domain.common.UserType.CUSTOMER;
@@ -24,25 +27,39 @@ public class SignInApplication {
 	private final CustomerService customerService;
 	private final SellerService sellerService;
 	private final JwtAuthenticationProvider provider;
+	private final RedisTemplate<String, String> redisTemplate;
 
-	public String customerLogInToken(SignInForm form) {
+	public AuthResponse customerLogInToken(SignInForm form, HttpServletResponse response) {
 		// 1. 로그인 가능 여부
 		Customer customer = customerService.findValidCustomer(form.getEmail(), form.getPassword())
 				.orElseThrow(() -> new CustomException(LOGIN_CHECK_FAIL));
 
-		// 2. 토큰을 발행
+		// 2. Access & Refresh Token 발급
+		String accessToken = provider.createAccessToken(customer.getEmail(), customer.getId(), CUSTOMER);
+		String refreshToken = provider.createRefreshToken(customer.getEmail(), customer.getId(), CUSTOMER);
 
-		// 3. 토큰을 Response 한다
-		return provider.createToken(customer.getEmail(), customer.getId(), CUSTOMER);
+		// 3. Redis에 Refresh Token 저장
+		redisTemplate.opsForValue().set(form.getEmail(), refreshToken, 7, TimeUnit.DAYS);
+
+
+		// 4. Refresh Token을 HttpOnly Cookie에 설정
+		Cookie cookie = new Cookie("refreshToken", refreshToken);
+		cookie.setHttpOnly(true);
+		cookie.setPath("/");
+		cookie.setMaxAge((int) TimeUnit.DAYS.toSeconds(7));
+		response.addCookie(cookie);
+
+		// 5. 토큰을 Response
+		return new AuthResponse(accessToken);
 	}
 
 	// 로그인 및 토큰 발행
-	public String sellerLogInToken(SignInForm form)	{
+	public String sellerLogInToken(SignInForm form) {
 		// 1. 로그인 (email, password)
 		Seller seller = sellerService.findValidSeller(form.getEmail(), form.getPassword())
 				.orElseThrow(() -> new CustomException(LOGIN_CHECK_FAIL));
 
 		// 2. 토큰 발행 및 return
-		return provider.createToken(seller.getEmail(), seller.getId(), SELLER);
+		return provider.createAccessToken(seller.getEmail(), seller.getId(), SELLER);
 	}
 }
---
+++ user-api/src/main/java/com/zerobase/cms/user/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.zerobase.cms.user.config;
+
+import com.zerobase.cms.user.config.filter.CustomerFilter;
+import com.zerobase.domain.config.JwtAuthenticationProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity // SpringSecurity 활성화
+public class SecurityConfig {
+
+	private final JwtAuthenticationProvider jwtAuthenticationProvider;
+
+	public SecurityConfig(JwtAuthenticationProvider jwtAuthenticationProvider) {
+		this.jwtAuthenticationProvider = jwtAuthenticationProvider;
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable() // csrf 비활성화 (API 호출용), Jwt 사용시 필요 없음
+				// CSRF 보호 : 세션 기반 인증에서는 필요하지만 JWT는 상태를 저장하지 않는 무상태 방식이므로 불필요
+				.authorizeRequests(
+						auth -> auth.requestMatchers(
+										"/signup/**",
+										"/signIn/**",
+										"/auth/**",
+										"/api/auth/**",
+										"swagger-ui/**",
+										"/v3/api-docs/**"
+								).permitAll()
+								.anyRequest().authenticated() // 그 외 요청은 인증 필요
+				)
+				.addFilterBefore(new CustomerFilter(jwtAuthenticationProvider), UsernamePasswordAuthenticationFilter.class)
+				.sessionManagement(session -> session.sessionCreationPolicy((SessionCreationPolicy.STATELESS)));
+				// 세션 사용 안 함
+		return http.build();
+	}
+}
